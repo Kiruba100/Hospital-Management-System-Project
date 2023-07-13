9ed04abd01dae8e014a9542ef322933f679a92d1
Kiruba100 <125954764+Kiruba100@users.noreply.github.com>
2023-07-13 14:37:21 +0530

    Add files via upload

---
+++ Day6/AboutUs.js
@@ -0,0 +1,40 @@
+import React from "react";
+// import Navbar from './Navbar';
+import Navi from "./Navbar";
+
+function AboutUs() {
+    return (
+      <div>
+        <Navi/>
+        <h2>About Our Hospital Management System</h2>
+        <p>
+          The Hospital Management System is a comprehensive solution designed to streamline and automate the administrative and operational tasks of a hospital or healthcare facility. Our system aims to improve efficiency, enhance patient care, and optimize resource utilization.
+        </p>
+        <h3>Key Features</h3>
+        <ul>
+          <li>Patient management: Registration, scheduling, and medical history</li>
+          <li>Doctor management: Profiles, appointments, and availability</li>
+          <li>Appointment booking and management</li>
+          <li>Electronic health records (EHR) management</li>
+          <li>Pharmacy and inventory management</li>
+          <li>Billing and payment processing</li>
+          <li>Reporting and analytics</li>
+        </ul>
+        <h3>Benefits</h3>
+        <ul>
+          <li>Improved operational efficiency</li>
+          <li>Enhanced patient experience</li>
+          <li>Reduced administrative workload</li>
+          <li>Better resource allocation</li>
+          <li>Accurate and accessible patient records</li>
+          <li>Streamlined billing and revenue management</li>
+        </ul>
+        <p>
+          At XYZ Hospital, we are committed to providing the best healthcare services, and our Hospital Management System plays a vital role in achieving that goal. If you have any inquiries or would like to learn more about our system, please feel free to contact us.
+        </p>
+      </div>
+    );
+  }
+  
+  export default AboutUs;
+  
---
+++ Day6/footer.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+                <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Quick Links</h4>
+            <ul>
+              <li><a href="/">Twitter</a></li>
+              <li><a href="/about">Facebook</a></li>
+              <li><a href="/contact">Instagram</a></li>
+            </ul>
+          </div>
+          <div className="col-md-6">
+            <h4>Contact Information</h4>
+            <ul>
+              <li><i className="fas fa-map-marker-alt"></i> 123 Hospital Street, City, Country</li>
+              <li><i className="fas fa-phone"></i> +1 123-456-7890</li>
+              <li><i className="fas fa-envelope"></i> info@hospital.com</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <p className="text-center">&copy; 2023 Hospital Management System. All rights reserved.</p>
+      </div>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
